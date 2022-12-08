c68ab315250925c191dd7fb2704ff4ed4378fd6c
Ledeya Gankova <gankoval.a.0108@mail.ru>
2022-12-08 17:42:54 +0300

    Задача 3 к семинару (find max)

---
+++ Zadacha_002/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Введите два числа:");
+int a, b;
+Console.WriteLine("Введите первое число: ");
+a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+b = Convert.ToInt32(Console.ReadLine());
+
+bool int_1 = a > b;
+bool int_2 = a == b;
+bool int_3 = a < b;
+
+if (int_1)
+{
+    Console.WriteLine($"Число {a} больше, чем {b}");
+}
+else if (int_2)
+{
+    Console.WriteLine($"Числа {a} и {b} равны");
+}
+else 
+{
+    Console.WriteLine($"Число {b} больше, чем {a}");
+}
+
