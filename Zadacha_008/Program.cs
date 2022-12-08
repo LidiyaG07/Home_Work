3dcdb2aedfc608332256ef3ea09f85a7b68c9018
Ledeya Gankova <gankoval.a.0108@mail.ru>
2022-12-08 22:29:19 +0300

    Zadacha на вывод четных чисел до N(исправила неточность)

--- Zadacha_008/Program.cs
+++ Zadacha_008/Program.cs
@@ -1,7 +1,7 @@
 ﻿Console.Write("Введите целое число: ");
 int n = int.Parse(Console.ReadLine());
 int i;
-for (i = 0; i<=n; i++ )
+for (i = 1; i<=n; i++ )
    if (i % 2 == 0)
    {
     Console.WriteLine(i);
