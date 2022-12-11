b485e9f1070d681177369490ef306a5c405db636
Ledeya Gankova <gankoval.a.0108@mail.ru>
2022-12-12 00:18:40 +0300

    Zadacha013 на трехзначные числа

---
+++ Zadacha_013/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите трезначное число: ");
+string numbers = Console.ReadLine();
+int num = Convert.ToInt32(numbers);
+
+if (num <99 )
+{
+    Console.Write("Третьей цифры нет");
+} else
+{
+    Console.Write(num.ToString()[2]);
+}
