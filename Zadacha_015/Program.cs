92102e70613f1bff670dbcec68752cd1ce221618
Ledeya Gankova <gankoval.a.0108@mail.ru>
2022-12-12 12:17:48 +0300

    Zadacha_015

---
+++ Zadacha_015/Program.cs
@@ -0,0 +1,23 @@
+﻿/* Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+6 -> да
+7 -> да
+1 -> нет
+*/
+
+Console.WriteLine("Введите цифру");
+string num = Console.ReadLine();
+int number = Convert.ToInt32(num);
+
+if (number >= 6 && number <= 7)
+{
+    Console.Write("Да");
+    
+}
+else if (number <6)
+{
+    Console.Write("Нет");
+}
+else
+{
+    Console.WriteLine("В недели только 7 дней!");
+}
