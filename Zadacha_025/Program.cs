ca7a6b3d56eeb2334ada35d709bf5e410c3d2cc5
Ledeya Gankova <gankoval.a.0108@mail.ru>
2022-12-16 17:48:08 +0300

    Zadacha_025

---
+++ Zadacha_025/Program.cs
@@ -0,0 +1,24 @@
+﻿/* 
+Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16
+*/
+
+
+Console.WriteLine("Введите число первое число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число второе число");
+int b = Convert.ToInt32(Console.ReadLine());
+GetSumm(a, b);
+
+void GetSumm(int a, int b)
+{
+    int result = 1;
+    for (int i = 0; i < b; i++)
+        {
+            result = result *a;
+                
+        }
+        Console.WriteLine(result);
+}
+
