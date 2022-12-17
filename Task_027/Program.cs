2a78858c5ae54f889d18b544af002046e1475461
Ledeya Gankova <gankoval.a.0108@mail.ru>
2022-12-17 23:31:51 +0300

    Task k seminaru

---
+++ Task_027/Program.cs
@@ -0,0 +1,31 @@
+﻿/* Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12 */
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int len = NumberLen(number);
+SummNumbers(number, len);
+
+int NumberLen(int number)
+{
+    int index = 0;
+    while (number > 0)
+    {
+        number /= 10;
+        index++;
+    }
+    return index;
+}
+
+void SummNumbers(int b, int len)
+{
+    int result = 0;
+    for (int i = 1; i <= len; i++)
+    {
+        result += b % 10;
+        b /= 10;
+    }
+    Console.WriteLine(result);
+}
---
+++ Task_029/Program.cs
@@ -0,0 +1,16 @@
+﻿/* Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33] 
+*/
+
+Console.WriteLine("Массив из 8 элементов: ");
+int[] array = new int[8];
+var random = new Random();
+int result = 0;
+
+for (int i = 1; i < array.Length; i++)
+{
+    array[i] = random.Next(1, 100);
+    result = array[i];
+}
+Console.WriteLine($"[{String.Join(", ", array)}]");
