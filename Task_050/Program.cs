e317333cefa0b45ce3628bafbb39fec5b3706e53
Ledeya Gankova <gankoval.a.0108@mail.ru>
2023-01-07 12:34:55 +0300

    Task k seminaru 7

---
+++ Task_050/Program.cs
@@ -0,0 +1,56 @@
+﻿/*
+Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+и возвращает значение этого элемента или же указание, что такого элемента нет.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+17 -> такого числа в массиве нет
+*/
+
+Console.WriteLine("Введите позию в строке: ");
+int positionX = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите позию в столбце: ");
+int positionY = Convert.ToInt32(Console.ReadLine());
+int[,] array = new int[3, 4];
+Console.WriteLine("Двумерный массив:");
+GetProgram(array);
+PrintArray(array);
+FindNumber(positionX, positionY, array);
+
+void GetProgram(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 100);
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write(" ");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.Write(" ");
+        Console.WriteLine("");
+    }
+}
+
+void FindNumber(int positionX, int positionY, int[,] array)
+{
+    if ((positionX < 0 && positionX > array.GetLength(0)) && (positionY < 0 && positionY > array.GetLength(1)))
+    {
+        Console.WriteLine("Такого числа в массиве нет!");
+    }
+    else
+    {
+        Console.WriteLine($"Число в масиве: {array[positionX, positionY]}");
+    }
+}
---
+++ Task_052/Program.cs
@@ -0,0 +1,49 @@
+﻿/*
+Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+*/
+
+// Задаем массив
+ Console.WriteLine("Двумерный массив:");
+int[,] array = new int[3, 4];
+int[] summ = new int[array.GetLength(1)];
+
+// Задаем численность в массиве
+Random rnd = new();
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        array[i, j] = rnd.Next(1, 10);
+    }
+}
+
+// Вывод массива
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    Console.Write(" ");
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        Console.Write(String.Format("{0,5:0.##}", array[i, j]));
+    }
+    Console.WriteLine();
+}
+
+// Получение среднеарифмитического
+for (int j = 0; j < array.GetLength(1); j++)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        summ[j] += array[i, j];
+    }
+}
+
+// Вывод среднеарифмитического
+for (int i = 0; i < summ.Length; i++)
+{
+    Console.WriteLine(String.Format("среднеарифмитическое значение: " + "{0,5:0.##}", (double)summ[i] / array.GetLength(0)));
+}
